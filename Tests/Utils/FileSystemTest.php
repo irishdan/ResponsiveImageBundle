9d96764552efff45ed37ab3a8f239bee9483bac9
irishdan <danntastic@gmail.com>
2016-04-26 09:52:16 +0700

    Add test class

---
+++ Tests/Utils/FileSystemTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danielbyrne
+ * Date: 26/04/2016
+ * Time: 09:31
+ */
+
+namespace ResponsiveImageBundle\Tests\Utils;
+
+
+class FileSystemTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp() {
+
+    }
+
+    public function testDirectoryExists()
+    {
+
+    }
+
+    public function testDeleteDirectory()
+    {
+
+    }
+
+    public function testDeleteFile()
+    {
+
+    }
+
+    public function testUploadedFilePath()
+    {
+
+    }
+
+    public function testStyleDirectoryPath()
+    {
+
+    }
+
+    public function testStyleFilePath()
+    {
+
+    }
+
+    public function testGetFilenameFromPath()
+    {
+
+    }
+
+    public function testStyleWebPath()
+    {
+
+    }
+}
