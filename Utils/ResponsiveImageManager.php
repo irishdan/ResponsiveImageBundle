32b836f30d3d5d58312285614ab5fce0d52bd7fa
irishdan <danntastic@gmail.com>
2016-04-29 18:49:33 +0700

    Work in progress

--- Utils/ResponsiveImageManager.php
+++ Utils/ResponsiveImageManager.php
@@ -38,6 +38,11 @@ class ResponsiveImageManager
     private $dispatcher;
 
     /**
+     * @var array
+     */
+    private $imagePaths = [];
+
+    /**
      * ImageManager constructor.
      * @param $imager
      * @param $config
@@ -51,7 +56,15 @@ class ResponsiveImageManager
         $this->dispatcher = $dispatcher;
     }
 
-    public function createStyledImage($imageObject, $styleName) {
+    /**
+     * @param $imageObject
+     * @param $styleName
+     * @param bool $storePath
+     * @param bool $tmp
+     * @return mixed
+     */
+    public function createStyledImage($imageObject, $styleName, $storePath = FALSE, $tmp = FALSE)
+    {
         $system = $this->system;
         $stylePath = $system->styleDirectoryPath($styleName);
         $originalPath = $system->uploadedFilePath($imageObject->getPath());
@@ -60,6 +73,10 @@ class ResponsiveImageManager
 
         $crop = empty($imageObject) ? null : $imageObject->getCropCoordinates();
         $image = $this->imager->createImage($originalPath, $stylePath, $style, $crop);
+
+        if ($storePath) {
+            $this->imagePaths[] = $stylePath;
+        }
 
         // Despatch event to any listeners.
         $event = new ImageEvent($imageObject, $style);
@@ -74,12 +91,14 @@ class ResponsiveImageManager
     /**
      * @param ResponsiveImageInterface $image
      */
-    public function createAllStyledImages(ResponsiveImageInterface $image) {
+    public function createAllStyledImages(ResponsiveImageInterface $image)
+    {
+        $this->imagePaths = [];
         $filename = $image->getPath();
         $styles = $this->styleManager->getAllStyles();
         if (!empty($filename)) {
             foreach ($styles as $stylename => $style) {
-                $this->createStyledImage($image, $stylename);
+                $this->createStyledImage($image, $stylename, TRUE);
             }
         }
     }
@@ -87,10 +106,38 @@ class ResponsiveImageManager
     /**
      * @param ResponsiveImageInterface $image
      */
-    public function deleteAllStyledImages(ResponsiveImageInterface $image) {
+    public function deleteAllStyledImages(ResponsiveImageInterface $image)
+    {
         $filename = $image->getPath();
         if (!empty($filename)) {
             $this->styleManager->deleteImageStyledFiles($filename);
+        }
+    }
+
+    /**
+     *  Transfer files to S3 bucket
+     */
+    public function transferToS3()
+    {
+        $commands = array();
+        $commands[] = $s3Client->getCommand('PutObject', array(
+            'Bucket' => 'SOME_BUCKET',
+            'Key' => 'photos/photo01.jpg',
+            'Body' => fopen('/tmp/photo01.jpg', 'r'),
+        ));
+        $commands[] = $s3Client->getCommand('PutObject', array(
+            'Bucket' => 'SOME_BUCKET',
+            'Key' => 'photos/photo02.jpg',
+            'Body' => fopen('/tmp/photo02.jpg', 'r'),
+        ));
+
+        // Execute an array of command objects to do them in parallel
+        $s3Client->execute($commands);
+
+        // Loop over the commands, which have now all been executed
+        foreach ($commands as $command) {
+            $result = $command->getResult();
+            // Do something with result
         }
     }
 }
