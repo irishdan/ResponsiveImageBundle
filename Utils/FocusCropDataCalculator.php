756099688d68a934058db3edb5602e3c09d106d6
irishdan <danntastic@gmail.com>
2017-05-31 09:35:27 +0100

    Refactor

---
+++ Utils/FocusCropDataCalculator.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace ResponsiveImageBundle\Utils;
+
+class FocusCropDataCalculator
+{
+    private $cropCoordinates;
+    private $focusCoordinates;
+    private $styleWidth;
+    private $styleHeight;
+
+    public function __construct($cropCoordinates, $focusCoordinates, $styleWidth, $styleHeight)
+    {
+        $this->cropCoordinates = $cropCoordinates;
+        $this->focusCoordinates = $focusCoordinates;
+        $this->styleWidth = $styleWidth;
+        $this->styleHeight = $styleHeight;
+    }
+
+    public function getFocusCropData()
+    {
+        // If there is a focus rectangle,
+        // We have the image shape (or crop rectangle),
+        // and we have the the final image style rectangle.
+        //
+        // The image style shape should be as large as possible so,
+        // there are three possibilities:
+        // 1: The style rectangle fits inside the crop rectangle vertically.
+        //    The sides of the image will be cropped.
+        // 2: The style rectangle fits inside the crop rectangle horizontally.
+        //    The top and bottom of the image will be cropped.
+        // 3: The style rectangle fits inside the crop rectangle exactly.
+        //    no cropping in required
+        //
+        // To determine which type of cropping should be used, the aspect-ratio of the image/crop rectangle ($imageAspectRatio)
+        // and the aspect-ratio of the style ($styleAspectRatio) are compared.
+        // 1: $imageAspectRatio > $styleAspectRatio
+        // 2: $imageAspectRatio < $styleAspectRatio
+        // 3: $imageAspectRatio === $styleAspectRatio
+
+        $newWidth = $this->getLength('x', $this->cropCoordinates);
+        $newHeight = $this->getLength('y', $this->cropCoordinates);
+
+        // Find out what type of style crop we are dealing with.
+        $imageAspectRatio = $newWidth / $newHeight;
+        $styleAspectRatio = $this->styleWidth / $this->styleHeight;
+
+        if ($imageAspectRatio > $styleAspectRatio) {
+            $axis = 'x';
+        } else {
+            $axis = 'y';
+        }
+
+        if (!empty($axis)) {
+            $this->calculateAxisCropData($axis, $this->cropCoordinates, $styleAspectRatio, $newWidth, $newHeight);
+        }
+
+        return [];
+    }
+
+    protected function calculateAxisCropData($axis = 'x', $cropCoordinates, $aspectRatio, $newWidth, $newHeight)
+    {
+        $cropWidth = ($axis == 'y') ? $newWidth : $newHeight * $aspectRatio;
+        $cropHeight = ($axis == 'y') ? $newWidth / $aspectRatio : $newHeight;
+
+        $cropXOffset = ($axis == 'y') ? $cropCoordinates[0] : $this->getFloatingOffset('x', $cropWidth, $cropCoordinates[0]);
+        $cropYOffset = ($axis == 'y') ? $this->getFloatingOffset('y', $cropHeight, $cropCoordinates[1]) : $cropCoordinates[1];
+
+        return [
+            'width' => $cropWidth,
+            'height' => $cropHeight,
+            'x' => $cropXOffset,
+            'y' => $cropYOffset,
+        ];
+    }
+
+    protected function getFloatingOffset($axis = 'y', $point, $start)
+    {
+        $offset = $this->findFocusOffset($axis, $point);
+        $offset = $offset + $start;
+
+        return $offset;
+    }
+
+    protected function getFocusPointForAxis($axis, $type = 'near')
+    {
+        $cropX1 = $this->cropCoordinates[0];
+        $cropY1 = $this->cropCoordinates[1];
+        $cropX2 = $this->cropCoordinates[2];
+        $cropY2 = $this->cropCoordinates[3];
+
+        $focusX1 = $this->focusCoordinates[0];
+        $focusY1 = $this->focusCoordinates[1];
+        $focusX2 = $this->focusCoordinates[2];
+        $focusY2 = $this->focusCoordinates[3];
+
+        if ($type == 'near') {
+            $point = ${'focus' . $axis . '1'} - ${'crop' . $axis . '1'};
+        } else {
+            $point = ${'focus' . $axis . '2'} - ${'crop' . $axis . '1'};
+        }
+
+        return $point;
+    }
+
+    /**
+     * Calculates the offset needed to keep focus rectangle in view with optimal position.
+     *
+     * @param string $axis
+     * @param        $cropLength
+     * @return mixed
+     */
+    protected function findFocusOffset($axis = 'x', $cropLength)
+    {
+        $axis = ucfirst($axis);
+
+        // Get the crop and focus information,
+        // and the length.
+        $imageLength = $this->getLength($axis, $this->cropCoordinates);
+
+        // If there are no focus coordinates the image should be cropped from center.
+        if (empty($this->focusCoordinates)) {
+            return ($imageLength - $cropLength) / 2;
+        }
+
+        // Offsetting on either the x or the y axis.
+        // Subtract the crop rectangle.
+        $focusNear = $this->getFocusPointForAxis($axis, 'near');
+        $focusFar = $this->getFocusPointForAxis($axis, 'far');
+
+        $focusLength = $focusFar - $focusNear;
+        $focusCenter = round(($focusNear + $focusFar) / 2);
+
+        // There are two possibilities.
+        // 1: The focus area is longer then the desired crop length.
+        //    In this case we simple center ont he crop area on the the center of the focus area.
+        //    Both sides of the image will be clipped, and both sides of the crop area will be missing a piece.
+        // 2: In most cases the focus rectangle will fit nicely within the crop area.
+        //    We must find the optimal position to crop from.
+        $focusType = $focusLength >= $cropLength ? 'in' : 'out';
+
+        // First case.
+        if ($focusType == 'in') {
+            return $focusCenter - ($cropLength / 2);
+        } // Second case.
+        else {
+            // We will find a range of valid offsets,
+            $validOffsets = $this->getValidOffsets($focusNear, $focusFar, $cropLength, $imageLength);
+
+            // Return the most optimal offset.
+            return $this->getOptimalOffset($validOffsets);
+        }
+    }
+
+    protected function getOptimalOffset(array $validOffsets)
+    {
+        $offset = 0;
+
+        if (!empty($validOffsets)) {
+            asort($validOffsets);
+            $offsets = array_keys($validOffsets);
+            $offset = reset($offsets);
+        }
+
+        return $offset;
+    }
+
+    protected function getValidOffsets($focusNear, $focusFar, $cropLength, $imageLength)
+    {
+        $nearGap = $focusNear;
+        $farGap = $imageLength - $focusFar;
+        $offFactor = $nearGap / $farGap;
+
+        // Will need the maximum and minimum offset also.
+        $maxOffset = $imageLength - $cropLength;
+        $minOffset = 0;
+
+        $validOffsets = [];
+        for ($i = $minOffset; $i <= $maxOffset; $i++) {
+            if ($this->isInBounds($i, $cropLength, $imageLength, $focusNear, $focusFar)) {
+                // Need a factor of near / far to compare to offFactor.
+                // Closest to that wins.
+                $near = $focusNear - $i;
+                $far = ($i + $cropLength) - $focusFar;
+                if ($near != 0 && $far != 0) {
+                    $optimalFactor = ($near / $far) / $offFactor;
+                    $optimalFactor = abs($optimalFactor);
+
+                    $theTest = abs($optimalFactor - 1);
+                    $validOffsets[$i] = $theTest;
+                }
+            }
+        }
+
+        return $validOffsets;
+    }
+
+    /**
+     * Tests if a given offset is valid.
+     * Valid offsets cropped images will include the focus rectangle and will not fall outside of the original image.
+     *
+     * @param $point
+     * @param $cropLength
+     * @param $imageLength
+     * @param $focusNear
+     * @param $focusFar
+     * @return bool
+     */
+    protected function isInBounds($point, $cropLength, $imageLength, $focusNear, $focusFar)
+    {
+        $inBounds = false;
+        if ($point + $cropLength <= $imageLength && $point <= $focusNear && $point + $cropLength >= $focusFar) {
+            $inBounds = true;
+        }
+
+        return $inBounds;
+    }
+
+    /**
+     * Gets vertical or horizontal length between two coordinates (x, y, x2, y2).
+     *
+     * @param string $type
+     * @param array  $coords
+     * @return mixed
+     */
+    protected function getLength($type = 'x', array $coords)
+    {
+        $type = strtolower($type);
+        if ($type == 'x') {
+            return $coords[2] - $coords[0];
+        } else {
+            return $coords[3] - $coords[1];
+        }
+    }
+}
--- Utils/ImageMaker.php
+++ Utils/ImageMaker.php
@@ -99,7 +99,7 @@ class ImageMaker
         });
     }
 
-    public function setImg($source, $driver = 'gd')
+    public function setImage($source, $driver = 'gd')
     {
         if (empty($this->manager)) {
             $this->manager = new ImageManager(['driver' => $driver]);
@@ -119,7 +119,7 @@ class ImageMaker
      */
     public function createImage($source, $destination, array $style = [], $cropFocusCoords = null)
     {
-        $this->setImg($source, $this->driver);
+        $this->setImage($source, $this->driver);
         $this->setStyleData($style);
 
         if (!empty($cropFocusCoords)) {
@@ -139,44 +139,18 @@ class ImageMaker
                 // just cut out the crop rectangle.
                 if (empty($this->getCoordinates('focus'))) {
                     $this->doCropRectangle();
-                }
-                // If there is a crop rectangle,
-                // We have the image shape (or crop rectangle),
-                // and we have the the final image style rectangle.
-                // 
-                // The image style shape should be as large as possible so,
-                // there are three possibilities:
-                // 1: The style rectangle fits inside the crop rectangle vertically.
-                //    The sides of the image will be cropped.
-                // 2: The style rectangle fits inside the crop rectangle horizontally.
-                //    The top and bottom of the image will be cropped.
-                // 3: The style rectangle fits inside the crop rectangle exactly.
-                //    no cropping in required
-                //
-                // To determine which type of cropping should be used, the aspect-ratio of the image/crop rectangle ($imageAspectRatio)
-                // and the aspect-ratio of the style ($styleAspectRatio) are compared.
-                // 1: $imageAspectRatio > $styleAspectRatio
-                // 2: $imageAspectRatio < $styleAspectRatio
-                // 3: $imageAspectRatio === $styleAspectRatio
-                else {
-                    $cropCoords = $this->getCoordinates('crop');
-                    $newWidth = $this->getLength('x', $cropCoords);
-                    $newHeight = $this->getLength('y', $cropCoords);
-                    $styleWidth = $this->styleData['width'];
-                    $styleHeight = $this->styleData['height'];
+                } else {
 
-                    // Find out what type of style crop we are dealing with.
-                    $imageAspectRatio = $newWidth / $newHeight;
-                    $styleAspectRatio = $styleWidth / $styleHeight;
+                    $focusOffsetFinder = new FocusCropDataCalculator(
+                        $this->getCoordinates('crop'),
+                        $this->getCoordinates('focus'),
+                        $this->styleData['width'],
+                        $this->styleData['height']
+                    );
 
-                    if ($imageAspectRatio > $styleAspectRatio) {
-                        $axis = 'x';
-                    } else {
-                        $axis = 'y';
-                    }
-
-                    if (!empty($axis)) {
-                        $this->calculateAxisOffsetAndCrop($axis, $cropCoords, $styleAspectRatio, $newWidth, $newHeight);
+                    $focusCropData = $focusOffsetFinder->getFocusCropData();
+                    if (!empty($focusCropData)) {
+                        $this->cropImage($focusCropData['width'], $focusCropData['height'], $focusCropData['x'], $focusCropData['y']);
                     }
                 }
 
@@ -193,30 +167,6 @@ class ImageMaker
         }
 
         return $this->saveImage($destination, $source);
-    }
-
-    protected function calculateAxisOffsetAndCrop($axis = 'x', $cropCoordinates, $aspectRatio, $newWidth, $newHeight)
-    {
-        // Check axis if not x or y throw invalidExceptionError.
-        if ($axis !== 'x' || $axis !== 'y') {
-            // @TODO: Throw invalid exception.
-        }
-
-        $cropWidth = ($axis == 'y') ? $newWidth : $newHeight * $aspectRatio;
-        $cropHeight = ($axis == 'y') ? $newWidth / $aspectRatio : $newHeight;
-
-        $cropXOffset = ($axis == 'y') ? $cropCoordinates[0] : $this->getFloatingOffset('x', $cropWidth, $cropCoordinates[0]);
-        $cropYOffset = ($axis == 'y') ? $this->getFloatingOffset('y', $cropHeight, $cropCoordinates[1]) : $cropCoordinates[1];
-
-        $this->cropImage($cropWidth, $cropHeight, $cropXOffset, $cropYOffset);
-    }
-
-    protected function getFloatingOffset($axis = 'y', $point, $start)
-    {
-        $offset = $this->findFocusOffset($axis, $point);
-        $offset = $offset + $start;
-
-        return $offset;
     }
 
     protected function cropImage($width, $height, $xOffset, $yOffset)
@@ -249,145 +199,6 @@ class ImageMaker
         }
     }
 
-    public function getFocusPointForAccess($axis, $type = 'near')
-    {
-        $cropCoords = $this->getCoordinates('crop');
-        $focusCoords = $this->getCoordinates('focus');
-
-        $cropX1 = $cropCoords[0];
-        $cropY1 = $cropCoords[1];
-        $cropX2 = $cropCoords[2];
-        $cropY2 = $cropCoords[3];
-
-        $focusX1 = $focusCoords[0];
-        $focusY1 = $focusCoords[1];
-        $focusX2 = $focusCoords[2];
-        $focusY2 = $focusCoords[3];
-
-        if ($type == 'near') {
-            $point = ${'focus' . $axis . '1'} - ${'crop' . $axis . '1'};
-        } else {
-            $point = ${'focus' . $axis . '2'} - ${'crop' . $axis . '1'};
-        }
-
-        return $point;
-    }
-
-    /**
-     * Calculates the offset needed to keep focus rectangle in view with optimal position.
-     *
-     * @param string $axis
-     * @param        $cropLength
-     * @return mixed
-     */
-    public function findFocusOffset($axis = 'x', $cropLength)
-    {
-        $axis = ucfirst($axis);
-
-        // Get the crop and focus information,
-        // and the length.
-        $cropCoords = $this->getCoordinates('crop');
-        $focusCoords = $this->getCoordinates('focus');
-        $imageLength = $this->getLength($axis, $cropCoords);
-
-        // If there are no focus coordinates the image should be cropped from center.
-        if (empty($focusCoords)) {
-            return ($imageLength - $cropLength) / 2;
-        }
-
-        // Offsetting on either the x or the y axis.
-        // Subtract the crop rectangle.
-        $focusNear = $this->getFocusPointForAccess($axis, 'near');
-        $focusFar = $this->getFocusPointForAccess($axis, 'far');
-
-        $focusLength = $focusFar - $focusNear;
-        $focusCenter = round(($focusNear + $focusFar) / 2);
-
-        // There are two possibilities.
-        // 1: The focus area is longer then the desired crop length.
-        //    In this case we simple center ont he crop area on the the center of the focus area.
-        //    Both sides of the image will be clipped, and both sides of the crop area will be missing a piece.
-        // 2: In most cases the focus rectangle will fit nicely within the crop area.
-        //    We must find the optimal position to crop from.
-        $focusType = $focusLength >= $cropLength ? 'in' : 'out';
-
-        // First case.
-        if ($focusType == 'in') {
-            return $focusCenter - ($cropLength / 2);
-        } // Second case.
-        else {
-            // We will find a range of valid offsets,
-            $validOffsets = $this->getValidOffsets($focusNear, $focusFar, $cropLength, $imageLength);
-
-            // Return the most optimal offset.
-            return $this->getOptimalOffset($validOffsets);
-        }
-    }
-
-    public function getOptimalOffset(array $validOffsets)
-    {
-        $offset = 0;
-
-        if (!empty($validOffsets)) {
-            asort($validOffsets);
-            $offsets = array_keys($validOffsets);
-            $offset = reset($offsets);
-        }
-
-        return $offset;
-    }
-
-    public function getValidOffsets($focusNear, $focusFar, $cropLength, $imageLength)
-    {
-        $nearGap = $focusNear;
-        $farGap = $imageLength - $focusFar;
-        $offFactor = $nearGap / $farGap;
-
-        // Will need the maximum and minimum offset also.
-        $maxOffset = $imageLength - $cropLength;
-        $minOffset = 0;
-
-        $validOffsets = [];
-        for ($i = $minOffset; $i <= $maxOffset; $i++) {
-            if ($this->isInBounds($i, $cropLength, $imageLength, $focusNear, $focusFar)) {
-                // Need a factor of near / far to compare to offFactor.
-                // Closest to that wins.
-                $near = $focusNear - $i;
-                $far = ($i + $cropLength) - $focusFar;
-                if ($near != 0 && $far != 0) {
-                    $optimalFactor = ($near / $far) / $offFactor;
-                    $optimalFactor = abs($optimalFactor);
-
-                    $theTest = abs($optimalFactor - 1);
-                    $validOffsets[$i] = $theTest;
-                }
-            }
-        }
-
-        return $validOffsets;
-    }
-
-    /**
-     * Tests if a given offset is valid.
-     * Valid offsets cropped images will include the focus rectangle and will not fall outside of the original image.
-     *
-     * @param $point
-     * @param $cropLength
-     * @param $imageLength
-     * @param $focusNear
-     * @param $focusFar
-     * @return bool
-     */
-    public function isInBounds($point, $cropLength, $imageLength, $focusNear, $focusFar)
-    {
-        $inBounds = false;
-        if ($point + $cropLength <= $imageLength && $point <= $focusNear && $point + $cropLength >= $focusFar) {
-            $inBounds = true;
-        }
-
-        return $inBounds;
-    }
-
     /**
      * Returns either the crop or focus rectangle coordinates.
      *
@@ -410,23 +221,6 @@ class ImageMaker
         }
 
         return $coords;
-    }
-
-    /**
-     * Gets vertical or horizontal length between two coordinates (x, y, x2, y2).
-     *
-     * @param string $type
-     * @param array  $coords
-     * @return mixed
-     */
-    public function getLength($type = 'x', array $coords)
-    {
-        $type = strtolower($type);
-        if ($type == 'x') {
-            return $coords[2] - $coords[0];
-        } else {
-            return $coords[3] - $coords[1];
-        }
     }
 
     /**
