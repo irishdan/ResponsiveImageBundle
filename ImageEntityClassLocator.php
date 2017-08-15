ac4e15b93ad37a05a51dcc84f38f1c1758b80d75
irishdan <danntastic@gmail.com>
2017-08-15 16:53:21 +0100

    Git add ImageEntityClassLocatorTest

--- ImageEntityClassLocator.php
+++ ImageEntityClassLocator.php
@@ -12,30 +12,62 @@ namespace IrishDan\ResponsiveImageBundle;
 
 use Symfony\Component\HttpKernel\Config\FileLocator;
 
+/**
+ * Class ImageEntityClassLocator
+ *
+ * The aim is to find entity class which implements ResponsiveImageInterface
+ *
+ * @package IrishDan\ResponsiveImageBundle
+ */
 class ImageEntityClassLocator
 {
     const RESPONSIVE_IMAGE_INTERFACE = 'IrishDan\ResponsiveImageBundle\ResponsiveImageInterface';
+    /**
+     * @var null|string
+     */
     protected $className = null;
+    /**
+     * @var FileLocator
+     */
     protected $fileLocator;
+    /**
+     * @var string
+     */
+    protected $entityDirectory = 'Entity';
 
-    public function __construct($bundles, FileLocator $fileLocator)
+    /**
+     * ImageEntityClassLocator constructor.
+     *
+     * @param             $bundles
+     * @param FileLocator $fileLocator
+     */
+    public function __construct($bundles = [], FileLocator $fileLocator)
     {
+        $this->bundles     = $bundles;
         $this->fileLocator = $fileLocator;
+    }
 
-        // The aim is to find entity class which implements ResponsiveImageInterface
-
+    /**
+     * @return mixed
+     */
+    public function getClassName()
+    {
         // Scan Bundle directories for entity directory
         if (empty($this->className)) {
-            foreach ($bundles as $key => $namespace) {
+            foreach ($this->bundles as $key => $namespace) {
 
                 $path = $this->fileLocator->locate('@' . $key);
-                $path .= 'Entity';
+                $path .= $this->entityDirectory;
+
                 if (file_exists($path)) {
                     // Remove the final part of the namespace
                     $namespaceParts = explode('\\', $namespace);
                     array_pop($namespaceParts);
-                    $namespaceParts[] = 'Entity';
+                    $namespaceParts[] = $this->entityDirectory;
                     $namespace        = implode('\\', $namespaceParts);
+
+                    // switch any /'s into \'s for namespaces
+                    $namespace = str_replace('/', '\\', $namespace);
 
                     $allFiles = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path));
                     $phpFiles = new \RegexIterator($allFiles, '/\.php$/');
@@ -44,7 +76,7 @@ class ImageEntityClassLocator
                         $className = substr($fileName, 0, -4);
                         $FQCN      = $namespace . '\\' . $className;
 
-                        // Try to load thew file as a class and determine if it implements the interface
+                        // Try to load the file as a class and determine if it implements the interface
                         try {
                             $reflect = new \ReflectionClass($FQCN);
                             if ($reflect->implementsInterface(self::RESPONSIVE_IMAGE_INTERFACE)) {
@@ -58,13 +90,15 @@ class ImageEntityClassLocator
                 }
             }
         }
+
+        return $this->className;
     }
 
     /**
-     * @return mixed
+     * @param string $entityDirectory
      */
-    public function getClassName()
+    public function setEntityDirectory($entityDirectory)
     {
-        return $this->className;
+        $this->entityDirectory = $entityDirectory;
     }
 }
---
+++ Tests/ImageEntityNameResolverTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of the IrishDan\ResponsiveImageBundle package.
+ *
+ * (c) Daniel Byrne <danielbyrne@outlook.com>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace IrishDan\ResponsiveImageBundle;
+
+
+class ImageEntityNameResolverTest extends \PHPUnit_Framework_TestCase
+{
+
+}
